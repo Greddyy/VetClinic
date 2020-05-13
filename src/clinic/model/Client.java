58072c8a4fdfe92ef910cbefd0b3b7737b79f279
Greddyy <55572482+Greddyy@users.noreply.github.com>
2020-05-13 17:25:57 +0300

    accidentally put client into clientDAO. Reset.

--- src/clinic/model/Client.java
+++ src/clinic/model/Client.java
@@ -1,4 +1,75 @@
 package clinic.model;
 
 public class Client {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private boolean admin;
+
+    public Client(int id, String username, String password, String email, boolean admin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.admin = admin;
+    }
+
+    public Client(String username, String password, String email, boolean admin) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.admin = admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientDAO{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
 }
